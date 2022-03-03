0a84072aecd815b713266ad1d36c78a74ee10d04
SuchiKhare <90444833+SuchiKhare@users.noreply.github.com>
2022-03-03 12:25:19 -0500

    updating Controller to Autowired Service

--- src/main/java/com/suchi/springbootstarter/course/CourseController.java
+++ src/main/java/com/suchi/springbootstarter/course/CourseController.java
@@ -1,23 +1,20 @@
 package com.suchi.springbootstarter.course;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
 	
+	@Autowired
+	private CourseService course_service;
+	
 	@RequestMapping("/courses")
 	public List<Course> getCourses(){
-		List<Course> course_lst = new ArrayList<Course>();
-		
-		course_lst.add(new Course(1, "Java", "Java"));
-		course_lst.add(new Course(2, "Cloud", "Cloud"));
-		course_lst.add(new Course(1, "ML", "ML"));
-		
-		return course_lst;
+		return course_service.getCourses();
 	}
 	
 }
