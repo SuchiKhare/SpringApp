047266ab91aa11d5d51b57c7adb719e07f4c128a
SuchiKhare <90444833+SuchiKhare@users.noreply.github.com>
2022-03-03 16:48:17 -0500

    Step7 CRUDRepository with Apache Derby

---
+++ src/main/java/com/suchi/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,7 @@
+package com.suchi.springbootstarter.course;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Integer>{
+
+}
--- src/main/java/com/suchi/springbootstarter/course/CourseService.java
+++ src/main/java/com/suchi/springbootstarter/course/CourseService.java
@@ -4,35 +4,42 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
-	//for time being hardcoded
+	@Autowired
+	private CourseRepository course_repo;
+	
 	private List<Course> courses = new ArrayList<Course>(Arrays.asList(new Course(1, "Java", "Java"),
 			new Course(2, "Cloud", "Cloud"),
 			new Course(3, "ML", "ML")));
 	
 	public List<Course> getCourses(){
+		List<Course> courses = new ArrayList<Course>();
+		course_repo.findAll().forEach(courses::add);
+		
 		return courses;
 	}
 	
 	public Course getCourse(int id){
 		//Lambda function
-		return courses.stream().filter(c -> (c.getCourseId() == id)).findFirst().get();
-		
+		//return courses.stream().filter(c -> (c.getCourseId() == id)).findFirst().get();
+		return course_repo.findOne(id);
 	}
 	
 	public void createCourse(Course c) {
-		courses.add(c);
+		//courses.add(c);
+		course_repo.save(c);
+		
 	}
 
 	public void updateCourse(int course_Id, Course course) {
-		for(Course c : courses) {
-			if (c.getCourseId() == course_Id) {
-				courses.set(course_Id, course);
-			}
-		}
-		
+		/*
+		 * for(Course c : courses) { if (c.getCourseId() == course_Id) {
+		 * courses.set(course_Id, course); } }
+		 */
+		course_repo.save(course);
 	}
 }
