694e4e617614b6f59e5a52090640921793c62467
SuchiKhare <90444833+SuchiKhare@users.noreply.github.com>
2022-03-03 13:56:47 -0500

    Step5 get course and post to create new course

--- src/main/java/com/suchi/springbootstarter/course/CourseController.java
+++ src/main/java/com/suchi/springbootstarter/course/CourseController.java
@@ -2,8 +2,14 @@ package com.suchi.springbootstarter.course;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
+import org.jboss.logging.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +22,16 @@ public class CourseController {
 	public List<Course> getCourses(){
 		return course_service.getCourses();
 	}
+	@RequestMapping("/course/{Id}")
+	public Course getCourse(@PathVariable("Id")  int course_Id){
+		return course_service.getCourse(course_Id);
+	}
 	
+	@RequestMapping(method = RequestMethod.POST, value = "/courses")
+	public void addCourse(@RequestBody Course course) {
+		//RequestBody is going to tell that request payload body is going to contain the json
+		//convert to instance Cource and send further.
+		course_service.createCourse(course);
+		
+	}
 }
--- src/main/java/com/suchi/springbootstarter/course/CourseService.java
+++ src/main/java/com/suchi/springbootstarter/course/CourseService.java
@@ -1,20 +1,29 @@
 package com.suchi.springbootstarter.course;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
+	//for time being hardcoded
+	private List<Course> courses = new ArrayList<Course>(Arrays.asList(new Course(1, "Java", "Java"),
+			new Course(2, "Cloud", "Cloud"),
+			new Course(3, "ML", "ML")));
 	
 	public List<Course> getCourses(){
-		List<Course> course_lst = new ArrayList<Course>();
+		return courses;
+	}
+	
+	public Course getCourse(int id){
+		//Lambda function
+		return courses.stream().filter(c -> (c.getCourseId() == id)).findFirst().get();
 		
-		course_lst.add(new Course(1, "Java", "Java"));
-		course_lst.add(new Course(2, "Cloud", "Cloud"));
-		course_lst.add(new Course(3, "ML", "ML"));
-		
-		return course_lst;
+	}
+	
+	public void createCourse(Course c) {
+		courses.add(c);
 	}
 }
